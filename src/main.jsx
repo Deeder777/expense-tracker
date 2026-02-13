795f239ec4c05600baa2762afcea39f4d67dfde9
elbeknurmatov <elbeknurmatov01@gmail.com>
2026-02-13 16:35:40 +0500

    enable PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,4 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+registerSW({ immediate: true })
+
 
